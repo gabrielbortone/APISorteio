5322ea1c844cd3ed2308b625cd51f82b469f87e9
Gabriel Bortone <newgabrielbortone@gmail.com>
2020-08-06 17:49:02 -0300

    Implementing the models

---
+++ APISorteio/Models/Endereco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APISorteio.Models
+{
+    public class Endereco
+    {
+        public int EnderecoId { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string País { get; set; }
+    }
+}
---
+++ APISorteio/Models/Participante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APISorteio.Models
+{
+    public class Participante
+    {
+        public int ParticipanteId { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Cargo { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public Endereco Endereco { get; set; }
+    }
+}
